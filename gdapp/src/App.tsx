1c6b98b3b0e4ba3599a87c0c60ccfa0ed271d1fe
Faris <fmhfg@umsystem.edu>
2024-04-17 13:01:00 -0500

    base app set up

---
+++ gdapp/src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>GD app</Typography>
+    </div>
+  );
+}
+
+export default App;
